c2a5c351f61cec418bce9572a06e5621ecdb2613
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2020-09-05 16:47:23 +0200

    remove full bright

--- BlackhawkRescueMission5.lua
+++ BlackhawkRescueMission5.lua
@@ -1,18 +1,5 @@
 --Not o7 property
 
--- Project MainDab Loaded!
-local s = Instance.new("PointLight", game.Players.LocalPlayer.Character.Head)
-s.Brightness = .3
-s.Range = 100
-
-game.Lighting.Changed:connect(function()
-game.Lighting.TimeOfDay = "14:00:00"
-game.Lighting.FogEnd = 9999
-game.Lighting.Brightness = 2
-game.Lighting.ColorCorrection.Brightness = 0.1
-game.Lighting.ColorCorrection.Saturation = 0.1
-game.Lighting.Bloom.Intensity = 0.1
-end)
 local ScreenGui = Instance.new("ScreenGui")
 local Frame = Instance.new("Frame")
 local TextLabel = Instance.new("TextLabel")
