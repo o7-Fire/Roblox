b94b622a888136ebac998eff86762cd72df68f08
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2020-09-10 03:58:07 +0200

    Update OverkillFullbright.lua

--- OverkillFullbright.lua
+++ OverkillFullbright.lua
@@ -1,1 +1,13 @@
 loadstring(game:HttpGet("https://pastebin.com/raw/6GrAQY9r"))()
+local s = Instance.new("PointLight", game.Players.LocalPlayer.Character.Head)
+s.Brightness = .3
+s.Range = 100
+
+game.Lighting.Changed:connect(function()
+game.Lighting.TimeOfDay = "14:00:00"
+game.Lighting.FogEnd = 9999
+game.Lighting.Brightness = 2
+game.Lighting.ColorCorrection.Brightness = 0.1
+game.Lighting.ColorCorrection.Saturation = 0.1
+game.Lighting.Bloom.Intensity = 0.1
+end)
