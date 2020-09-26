19e2e406cbc8f36899db5342639528f8962d36d3
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2020-09-26 23:39:46 +0200

    Update TPpoint.lua

--- TPpoint.lua
+++ TPpoint.lua
@@ -18,6 +18,7 @@ Frame.BorderSizePixel = 5
 Frame.Position = UDim2.new(0.686852515, 0, 0.576456368, 0)
 Frame.Size = UDim2.new(0, 284, 0, 164)
 Frame.Draggable = true
+Frame.Active = true
 
 TextLabel.Parent = Frame
 TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
