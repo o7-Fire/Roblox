7e4f0c5f569dc5f2f4983e7f94a79e8e3681f37e
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2020-09-27 00:07:20 +0200

    Create Blackhawk Rescue Mission 5.lua

---
+++ profile/Blackhawk Rescue Mission 5.lua
@@ -0,0 +1,10 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/o7-Fire/Roblox/master/BlackhawkRescueMission5.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/o7-Fire/Roblox/master/TPpoint.lua"))()
+game.Lighting.Changed:connect(function()
+game.Lighting.TimeOfDay = "14:00:00"
+game.Lighting.FogEnd = 9999
+game.Lighting.Brightness = 2
+game.Lighting.ColorCorrection.Brightness = 0.1
+game.Lighting.ColorCorrection.Saturation = 0.1
+game.Lighting.Bloom.Intensity = 0.1
+end)
