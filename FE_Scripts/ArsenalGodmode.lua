4444e9c570bac91992678f2b49b9e3739955f33c
Nexity <46597698+Nexity@users.noreply.github.com>
2020-12-28 21:18:44 +0800

    Update ArsenalGodmode.lua

--- FE_Scripts/ArsenalGodmode.lua
+++ FE_Scripts/ArsenalGodmode.lua
@@ -1,3 +1,4 @@
--- lmao
+-- this godmode makes it so your character are permanently dead
+-- you can move around and do emotes, but no one can damage you and you can't deal damage
 
 game:GetService("Players").LocalPlayer.Character.IsAPlayer:Destroy()
