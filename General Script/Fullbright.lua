f6724a7685244108df5c52f6f89698358f4c0300
Nexity <46597698+Nexity@users.noreply.github.com>
2021-02-02 13:04:54 +0800

    Update Fullbright.lua

--- General Script/Fullbright.lua
+++ General Script/Fullbright.lua
@@ -1,15 +1,23 @@
-
 print("gay")
-game:GetService("Lighting").Brightness = 1
-game:GetService("Lighting").FogEnd = 786543
-game:GetService("Lighting").GlobalShadows = false
-game:GetService("Lighting").Ambient = Color3.fromRGB(178, 178, 178)
-local function disable()
+local EnableFullbright = true
+
+local function Normalize()
    game:GetService("Lighting").Brightness = _G.NormalLightingSettings.Brightness
    game:GetService("Lighting").ClockTime = _G.NormalLightingSettings.ClockTime
    game:GetService("Lighting").FogEnd = _G.NormalLightingSettings.FogEnd
    game:GetService("Lighting").GlobalShadows = _G.NormalLightingSettings.GlobalShadows
    game:GetService("Lighting").Ambient = _G.NormalLightingSettings.Ambient
 end
---disable()
---uncomment above to disable
+
+loacl function Fullbright()
+   game:GetService("Lighting").Brightness = 1
+   game:GetService("Lighting").FogEnd = 786543
+   game:GetService("Lighting").GlobalShadows = false
+   game:GetService("Lighting").Ambient = Color3.fromRGB(178, 178, 178)
+end
+
+if EnableFullbright == true then
+   Fullbright()
+elseif EnableFullbright == false then
+   Normalize()
+end
