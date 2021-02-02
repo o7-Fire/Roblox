00a7ceaa23d66e4b75de0bd4705b20aa136cb334
Nexity <46597698+Nexity@users.noreply.github.com>
2021-02-02 22:51:00 +0800

    Create PlaneCrazyWater.lua

---
+++ SpecificGameScripts/PlaneCrazyWater.lua
@@ -0,0 +1,8 @@
+-- makes the map into water
+-- get a boat or submarine
+-- people might see your boat / submarine suddenly clip into terrain or submerging into terrain
+-- people can go onto your seats and clip into terrains with you
+
+local region = Region3.new(Vector3.new(-3750,-100,-2050), Vector3.new(2500,43,2050))
+region = region:ExpandToGrid(4)
+game.Workspace.Terrain:FillRegion(region, 4, Enum.Material.Water)
