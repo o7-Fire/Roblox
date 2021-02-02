fe7527a8d7fe2d6ac09446776aec33c836f500b2
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-02-02 05:58:31 +0100

    Update Fullbright.lua

--- General Script/Fullbright.lua
+++ General Script/Fullbright.lua
@@ -3,7 +3,7 @@ game:GetService("Lighting").Brightness = 1
 game:GetService("Lighting").FogEnd = 786543
 game:GetService("Lighting").GlobalShadows = false
 game:GetService("Lighting").Ambient = Color3.fromRGB(178, 178, 178)
-function disable():
+local function disable()
    game:GetService("Lighting").Brightness = _G.NormalLightingSettings.Brightness
    game:GetService("Lighting").ClockTime = _G.NormalLightingSettings.ClockTime
    game:GetService("Lighting").FogEnd = _G.NormalLightingSettings.FogEnd
