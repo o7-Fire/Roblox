1d7c3fc7def1b8f860585f168e38c23c7499f518
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-02-02 06:31:26 +0100

    Update Fullbright.lua

--- General Script/Fullbright.lua
+++ General Script/Fullbright.lua
@@ -1,20 +1,4 @@
---game.Lighting.Brightness = 10
---simple one ^ dont delete this 
-print("gay")
-local EnableFullbright = true
-
-local function Normalize()
-   game:GetService("Lighting").Brightness = _G.NormalLightingSettings.Brightness
-   game:GetService("Lighting").ClockTime = _G.NormalLightingSettings.ClockTime
-   game:GetService("Lighting").FogEnd = _G.NormalLightingSettings.FogEnd
-   game:GetService("Lighting").GlobalShadows = _G.NormalLightingSettings.GlobalShadows
-   game:GetService("Lighting").Ambient = _G.NormalLightingSettings.Ambient
-end
-
-loacl function Fullbright()
-
-
-
+game.Lighting.Changed:connect(function()
 game.Lighting.TimeOfDay = "14:00:00"
 game.Lighting.FogEnd = 9999
 game.Lighting.Brightness = 2
@@ -22,11 +6,4 @@ game.Lighting.GlobalShadows = false
 game.Lighting.ColorCorrection.Brightness = 0.1
 game.Lighting.ColorCorrection.Saturation = 0.1
 game.Lighting.Bloom.Intensity = 0.1
-
-end
-
-if EnableFullbright == true then
-   Fullbright()
-elseif EnableFullbright == false then
-   Normalize()
-end
+end)
