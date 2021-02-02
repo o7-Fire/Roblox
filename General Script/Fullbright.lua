6e7ca1a7e070ea9bac78c75f47befb78e095f2c8
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-02-02 06:28:07 +0100

    Update Fullbright.lua

--- General Script/Fullbright.lua
+++ General Script/Fullbright.lua
@@ -18,6 +18,7 @@ loacl function Fullbright()
 game.Lighting.TimeOfDay = "14:00:00"
 game.Lighting.FogEnd = 9999
 game.Lighting.Brightness = 2
+game.Lighting.GlobalShadows = false
 game.Lighting.ColorCorrection.Brightness = 0.1
 game.Lighting.ColorCorrection.Saturation = 0.1
 game.Lighting.Bloom.Intensity = 0.1
