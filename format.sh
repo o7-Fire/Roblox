1baa74955f2292a6e6afebaf89a40b2e36ed7396
Itzbenz <49940811+Itzbenz@users.noreply.github.com>
2021-02-07 04:04:35 +0100

    Update format.sh

--- format.sh
+++ format.sh
@@ -1,4 +1,4 @@
 chmod +x ./lua-format
 IFS=$'\n'; set -f
-for f in $(find . -name "*.lua"); do "./lua-format" -v -i $f; done
+for f in $(find . -name "*.lua"); do "./lua-format" -i $f; done
 unset IFS; set +f
