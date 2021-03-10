86dd637f1149211c8314c0c36e5cceb5a9806a69
Nexity <46597698+Nexity@users.noreply.github.com>
2021-03-10 20:38:36 +0800

    dont forget about this ok

---
+++ SpecificGameScripts/navalwarfare.lua
@@ -0,0 +1,15 @@
+for i, v in pairs(game.Workspace:GetChildren()) do
+pcall(function()
+
+if v.Owner.Value == "MrBlobFish13" then
+v.MainBody:MoveTo(CFrame.new(113.66, 22.28, 8121.96))
+end
+
+end)
+end
+
+--[[
+japan dock = -145.98, 70.59, -8068
+US dock = 113.66, 22.28, 8121.96
+auto win yes
+]]--
