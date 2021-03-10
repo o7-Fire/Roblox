1f6fba67426761956a3ab134c580143f5e7c97e9
Nexity <46597698+Nexity@users.noreply.github.com>
2021-03-10 17:05:55 +0800

    Create Hydroxide.lua

---
+++ Hydroxide.lua
@@ -0,0 +1,12 @@
+-- https://github.com/Upbolt/Hydroxide
+-- very cool
+
+local owner = "Upbolt"
+local branch = "revision"
+
+local function webImport(file)
+    return loadstring(game:HttpGetAsync(("https://raw.githubusercontent.com/%s/Hydroxide/%s/%s.lua"):format(owner, branch, file)), file .. '.lua')()
+end
+
+webImport("init")
+webImport("ui/main")
