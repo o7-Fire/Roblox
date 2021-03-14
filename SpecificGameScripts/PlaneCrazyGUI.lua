1e5f300e4527425b51d169cb8eeca14e450d6db5
Nexity <46597698+Nexity@users.noreply.github.com>
2021-03-14 21:46:00 +0800

    new ui pog
    now its usable

--- SpecificGameScripts/PlaneCrazyGUI.lua
+++ SpecificGameScripts/PlaneCrazyGUI.lua
@@ -1,127 +1,79 @@
--- made by Nexity#2106
--- no, this is not a copy script
-
-_G.Settings = {
-['Name'] = 'Plane Crazy o7',
-['Intro'] = false,
-['Keybind'] = 'P'
+local library = loadstring(game:HttpGet("https://raw.githubusercontent.com/GreenDeno/Venyx-UI-Library/main/source.lua"))()
+local venyx = library.new("Plane Crazy o7 (Made by Nexity#2106)", 5013109572)
+local themes = {
+Background = Color3.fromRGB(24, 24, 24),
+Glow = Color3.fromRGB(0, 0, 0),
+Accent = Color3.fromRGB(10, 10, 10),
+LightContrast = Color3.fromRGB(20, 20, 20),
+DarkContrast = Color3.fromRGB(14, 14, 14),  
+TextColor = Color3.fromRGB(255, 255, 255)
 }
-local hornpitch = {}
-local Library = loadstring(game:HttpGet("https://pastebin.com/raw/QPehPJ6m", true))()
+local page1 = venyx:addPage("Main Functions", 5012544693)
+local page2 = venyx:addPage("Random Scripts", 5012544693)
 
-local Tab1 = Library:CreateTab('Tab 1')
-Tab1:Label('Random Scripts')
-Tab1:Button('Ocean Map', function()
+local tab1 = page1:addSection("Honk")
+tab1:addSlider("Horn Pitch", 0.1, 0.1, 20, function(value)
+    for i,v in pairs(game.Workspace[tostring(game.Players.LocalPlayer) .. 'Aircraft']:GetChildren()) do
+        if v.Name == "Horn" then
+            v.Configuration.Pitch.Value = value
+        end
+    end
+end)
+
+local tptobase = page1:addSection("Base TP")
+tptobase:addTextbox("TP to Base", "NameHere", function(value, focusLost)
+    if focusLost then
+        for i,v in pairs(game.Workspace.BuildingZones:GetChildren()) do
+            if tostring(v.Owner.Value) == value then
+                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(v.Position.x, v.Position.y, v.Position.z - 75)
+            end
+        end
+    end
+end)
+
+local tpstuff = page1:addSection("Other Stuff")
+tpstuff:addTextbox("Kill Player", "NameHere", function(value, focusLost)
+    if focusLost then
+        for i,blocks in pairs(game.Workspace[tostring(game.Players.LocalPlayer) .. 'Aircraft']:GetChildren()) do
+            pcall(function()
+                if blocks.Name == "Seat" then
+                    blocks.Seat.CFrame = game.Players[value].Character.HumanoidRootPart.CFrame
+                    wait(0.8)
+                    blocks.Seat.CFrame = CFrame.new(blocks.Seat.Position.x, (blocks.Seat.Position.y-5000), blocks.Seat.Position.z)
+                end
+            end)
+        end
+    end
+end)
+tpstuff:addTextbox("TP Explosives to Player", "NameHere", function(value, focusLost)
+    if focusLost then
+        for i,blocks in pairs(game.Workspace[tostring(game.Players.LocalPlayer) .. 'Aircraft']:GetChildren()) do
+            pcall(function()
+                if blocks.Name == "ExplosiveBlock" then
+                    blocks.Decorate.CFrame = game.Players[value].Character.HumanoidRootPart.CFrame
+                    wait()
+                end
+                if blocks.Name == "ExplosiveBall" then
+                    blocks.Decorate.CFrame = game.Players[value].Character.HumanoidRootPart.CFrame
+                    wait()
+                end
+            end)
+        end
+    end
+end)
+
+local tab2 = page2:addSection("")
+tab2:addButton('Ocean Map', function()
     local region = Region3.new(Vector3.new(-3750,-100,-2050), Vector3.new(2500,43,2700))
     region = region:ExpandToGrid(4)
     game.Workspace.Terrain:FillRegion(region, 4, Enum.Material.Water)
 end)
-Tab1:Button('Gravity Gun', function()
+tab2:addButton('Gravity Gun', function()
     loadstring(game:HttpGet(('https://pastebin.com/raw/6cfa5Wxa'),true))()
 end)
-Tab1:Button('Infinite Yield', function()
+tab2:addButton('Infinite Yield', function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source'))()
 end)
-Tab1:Button("Destroy this GUI", function()
-    game.CoreGui.ShadowLib:Destroy()
-end)
-Tab1:Label('Made by Nexity#2106')
-
-local Tab2 = Library:CreateTab('Tab 2')
-Tab2:Label('Change All Horn Pitch')
-Tab2:TextBox('Pitch', 'Number', function(output)
-    hornpitch = output
-end)
-Tab2:Button('Change', function()
-    for i,v in pairs(game.Workspace[tostring(game.Players.LocalPlayer) .. 'Aircraft']:GetChildren()) do
-        if v.Name == "Horn" then
-            v.Configuration.Pitch.Value = hornpitch
-        end
-    end
-end)
-
-local Tab3 = Library:CreateTab('Tab 3')
-Tab3:Label('Teleport to Base')
-Tab3:Button('Refresh', function()
-    for i,v in pairs(game.CoreGui.ShadowLib.MainFrame['Tab 3']:GetChildren()) do
-        if v.ClassName == "ImageButton" then
-            if v.Title.Text == "Refresh" then
-                print("no")
-            else
-                v:Destroy()
-            end
-        end
-    end
-    for i,v in pairs(game.Workspace.BuildingZones:GetChildren()) do
-        if tostring(v.Owner.Value) == "nil" then
-            print("nil owner")
-        else
-            Tab3:Button(tostring(v.Owner.Value), function()
-                game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(v.Position.x, v.Position.y, v.Position.z - 75)
-            end)
-        end
-    end
-end)
-
-local Tab4 = Library:CreateTab('Tab 4')
-Tab4:Label('Kill with seats')
-Tab4:Button('Refresh', function()
-    for i,v in pairs(game.CoreGui.ShadowLib.MainFrame['Tab 4']:GetChildren()) do
-        if v.ClassName == "ImageButton" then
-            if v.Title.Text == "Refresh" then
-                print("no")
-            else
-                v:Destroy()
-            end
-        end
-    end
-    for i,v in pairs(game.Workspace.BuildingZones:GetChildren()) do
-        if tostring(v.Owner.Value) == "nil" then
-            print("nil owner")
-        else
-            Tab4:Button(tostring(v.Owner.Value), function()
-                for i,blocks in pairs(game.Workspace[tostring(game.Players.LocalPlayer) .. 'Aircraft']:GetChildren()) do
-                    pcall(function()
-                        if blocks.Name == "Seat" then
-                            blocks.Seat.CFrame = game.Players[tostring(v.Owner.Value)].Character.HumanoidRootPart.CFrame
-                            wait(0.8)
-                            blocks.Seat.CFrame = CFrame.new(blocks.Seat.Position.x, (blocks.Seat.Position.y-5000), blocks.Seat.Position.z)
-                        end
-                    end)
-                end
-            end)
-        end
-    end
-end)
-
-local Tab5 = Library:CreateTab('Tab 5')
-Tab5:Label('Teleport TNT to player')
-Tab5:Button('Refresh', function()
-    for i,v in pairs(game.CoreGui.ShadowLib.MainFrame['Tab 5']:GetChildren()) do
-        if v.ClassName == "ImageButton" then
-            if v.Title.Text == "Refresh" then
-                print("no")
-            else
-                v:Destroy()
-            end
-        end
-    end
-    for i,v in pairs(game.Workspace.BuildingZones:GetChildren()) do
-        if tostring(v.Owner.Value) == "nil" then
-            print("nil owner")
-        else
-            Tab5:Button(tostring(v.Owner.Value), function()
-                for i,blocks in pairs(game.Workspace[tostring(game.Players.LocalPlayer) .. 'Aircraft']:GetChildren()) do
-                    if blocks.Name == "ExplosiveBlock" then
-                        blocks.Decorate.CFrame = game.Players[tostring(v.Owner.Value)].Character.HumanoidRootPart.CFrame
-                        wait()
-                    end
-                    if blocks.Name == "ExplosiveBall" then
-                        blocks.Decorate.CFrame = game.Players[tostring(v.Owner.Value)].Character.HumanoidRootPart.CFrame
-                        wait()
-                    end
-                end
-            end)
-        end
-    end
+tab2:addButton("Destroy this GUI", function()
+    game.CoreGui["Plane Crazy o7"]:Destroy()
 end)
